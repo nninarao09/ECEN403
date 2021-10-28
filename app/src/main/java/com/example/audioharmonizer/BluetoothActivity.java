ffcee490bc84bca09ec2a3fb73197a185665ec0e
nninarao09 <ninarao09@gmail.com>
2021-10-28 16:15:13 -0500

    BT and skeleton Nabvar

--- app/src/main/java/com/example/audioharmonizer/BluetoothActivity.java
+++ app/src/main/java/com/example/audioharmonizer/BluetoothActivity.java
@@ -1,18 +1,36 @@
 package com.example.audioharmonizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Set;
 
 public class BluetoothActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.audioharmonizer.MESSAGE";
+    private static final int REQUEST_ENABLE_BT =0;
+    private static final int REQUEST_DISCOVER_BT = 1;
+
 
     // define the global variables
     TextView question1;
     Button next_Activity_button;
+
+    TextView mStatusBlueTv, mPairedTv;
+    ImageView mBlueIv;
+    Button mOnBtn, mOffBtn, mDiscoverableBtn, mPairedBtn;
+
+    BluetoothAdapter mBlueAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,10 +38,102 @@ public class BluetoothActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //**************bluetooth content below************************************************
+
+        /*
+        mStatusBlueTv = findViewById(R.id.statusBluetoothTv);
+        mPairedTv = findViewById(R.id.pairedTv);
+        mBlueIv = findViewById(R.id.bluetoothIv);
+        mOnBtn = findViewById(R.id.onBtn);
+        mOffBtn = findViewById(R.id.offBtn);
+        mDiscoverableBtn = findViewById(R.id.discoverableBtn);
+        mPairedBtn = findViewById(R.id.pairedBtn);
+
+        //adapter
+        mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        //check if bluetooth is available or not
+        if(mBlueAdapter == null){
+            mStatusBlueTv.setText("Bluetooth is not available");
+        } else{
+            mStatusBlueTv.setText("Bluetooth is available");
+        }
+
+        // set image according to blutooth status(on/off)
+        if(mBlueAdapter.isEnabled()){
+            mBlueIv.setImageResource(R.drawable.ic_action_on);
+        } else{
+            mBlueIv.setImageResource(R.drawable.ic_action_off);
+        }
+
+        //on btn click
+        mOnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(!mBlueAdapter.isEnabled()){
+                    showToast("Turning on Bluetooth...");
+                    //intenr to turn on bluetooth
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intent, REQUEST_ENABLE_BT);
+                } else{
+                    showToast("Bluetooth is already on");
+                }
+            }
+        });
+
+        //discover bluetooth btn
+        mDiscoverableBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(!mBlueAdapter.isDiscovering()){
+                    showToast("Making Your Device Discoverable");
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(intent, REQUEST_DISCOVER_BT);
+                }
+            }
+        });
+
+        //off btn click
+        mOffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(mBlueAdapter.isEnabled()){
+                    mBlueAdapter.disable();
+                    showToast("Turning off Bluetooth...");
+                    mBlueIv.setImageResource(R.drawable.ic_action_off);
+                } else{
+                    showToast("Bluetooth is already off");
+                }
+            }
+        });
+
+        //get paiered device click
+        mPairedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(mBlueAdapter.isEnabled()){
+                    mPairedTv.setText("Paired Devices");
+                    Set<BluetoothDevice> devices = mBlueAdapter.getBondedDevices();
+                    for(BluetoothDevice device: devices){
+                        mPairedTv.append("\nDevice: " + device.getName()+ "," + device);
+                    }
+
+                } else{
+                    //bluetooth is off so cant get paired devices
+                    showToast("Turn on bluetooth to get paired devices");
+                }
+
+            }
+        });
+        */
+
+
+        //*********************************************************************************
         // by ID we can use each component which id is assign in xml file
         // use findViewById() to get the Button
         next_Activity_button = (Button)findViewById(R.id.first_activity_button);
-        question1 = (TextView)findViewById(R.id.question1_id);
+        question1 = (TextView)findViewById(R.id.statusBluetoothTv);
 
         // In question1 get the TextView use by findViewById()
         // In TextView set question Answer for message
@@ -47,6 +157,29 @@ public class BluetoothActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch (requestCode){
+            case REQUEST_ENABLE_BT:
+                if(resultCode == RESULT_OK){
+                    //bluetooth is on
+                    mBlueIv.setImageResource(R.drawable.ic_action_on);
+                    showToast("Bluetooth is on");
+                } else {
+                    // user denied to turn bluetooth on
+                    showToast("couldn't turn on bluetooth");
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    // toeast message fucntion
+    private void showToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    //end of it
 }
 
 //public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/example/audioharmonizer/HomePageActivity.java
+++ app/src/main/java/com/example/audioharmonizer/HomePageActivity.java
@@ -1,9 +1,13 @@
 package com.example.audioharmonizer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,24 +16,33 @@ public class HomePageActivity extends AppCompatActivity {
 
     // define the global variable
     TextView question2;
-    // Add button Move to next Activity and previous Activity
-    Button next_button, previous_button;
+    Button next_button;
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
+        //**********************NavBar stuff**********************************
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //**********************************************************************
+
+
         // by ID we can use each component which id is assign in xml file
         // use findViewById() to get the both Button and textview
-
         next_button = (Button)findViewById(R.id.get_started);
-
-
-        // In question1 get the TextView use by findViewById()
-        // In TextView set question Answer for message
-
-        // Add_button add clicklistener
         next_button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v)
@@ -45,25 +58,15 @@ public class HomePageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
-        // Add_button add clicklistener
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
-//public class DisplayMessageActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_display_message);
-//
-//        // Get the Intent that started this activity and extract the string
-//        Intent intent = getIntent();
-//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-//
-//        // Capture the layout's TextView and set the string as its text
-//        TextView textView = findViewById(R.id.textView);
-//        textView.setText(message);
-//    }
-//}
